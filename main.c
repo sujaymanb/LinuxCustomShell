63d06c5a8afbc73868b39c50a633639b3b878b77
Utkarsh Patel <utkarshjp7@Gmail.com>
2016-11-13 20:59:32 -0500

    initialized the custom shell

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+
+	printf("Welcome to the stupid console\n");
+
+	char* input;
+
+	while (1) {
+		//calloc initializes memory block by setting each byte to 0.
+		input = (char *) calloc(10000, 1);
+
+		printf("stupid>>");
+		fflush(stdout);
+		read(0, input, 10000);
+
+
+		//Free the input variable before next read
+		free(input);
+		input = NULL;
+	}
+
+	return 0;
+}
