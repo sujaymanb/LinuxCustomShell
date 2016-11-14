79bd721f573b380b10175b1b3f277c3e1c48f924
Utkarsh Patel <utkarshjp7@Gmail.com>
2016-11-13 21:17:58 -0500

    added function stubs

---
+++ internalCmd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "internalCmd.h"
+
+void cd(char *path){
+
+}
+
+void path(){
+
+}
+
+void pathAdd(char *path){
+
+}
+
+void pathRemove(char *path){
+
+}
---
+++ internalCmd.h
@@ -0,0 +1,16 @@
+/*
+ * internalCmd.h
+ *
+ *  Created on: Nov 13, 2016
+ *      Author: root
+ */
+
+#ifndef INTERNALCMD_H_
+#define INTERNALCMD_H_
+
+void cd(char *path);
+void path();
+void pathAdd(char *path);
+void pathRemove(char *path);
+
+#endif /* INTERNALCMD_H_ */
